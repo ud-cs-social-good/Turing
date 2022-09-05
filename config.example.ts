e196b502cde90dca677a8ad857fb497f14509fa0
Maxwell Wang <maxwang@udel.edu>
2022-09-05 19:53:50 -0400

    example template

---
+++ config.example.ts
@@ -0,0 +1,41 @@
+export const BOT = {
+    TOKEN: "", //Your bot token goes here
+    CLIENT_ID: "",
+    NAME: "Turing"
+}
+
+export const DB = {
+    CONNECTION: "",
+    USERS: "users"
+}
+
+export const ROLES = {
+    PRESIDENT: "",
+    VICE_PRESIDENT: "",
+    SOCIAL_MEDIA: "",
+    VOLUNTEER_OUTRACH: "",
+    EBOARD: "",
+    VERIFIED: "",
+    LEVEL_ONE: "",
+    PROF: "",
+    ALUMNI: ""
+}
+
+export const CHANNELS = { // Channel IDs
+	ERROR_LOG: "",
+	SERVER_LOG: "",
+	MEMBER_LOG: "",
+	MOD_LOG: "",
+	FEEDBACK: "",
+	TURINGS_HOUSE: "",
+	ANNOUNCEMENTS: "",
+	ARCHIVE: "",
+	ROLE_SELECT: ""
+};
+
+export const FIRST_LEVEL = 10;
+export const GITHUB_TOKEN = "";
+export const GITHUB_PROJECT = "";
+export const MAINTAINERS = "";	// The current maintainers of this bot
+export const SEMESTER_ID = "";	// The current semester ID. i.e. s21
+export const BLACKLIST = [];
